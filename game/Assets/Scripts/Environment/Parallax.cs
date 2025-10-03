be8a130abd7030ea50ee7b31c6dd2c201ed93350
Leonardo Luz Fachel <leonardoluz.f02@gmail.com>
2025-10-03 17:52:26 -0300

    feat: add foreground parallax

--- game/Assets/Scripts/Environment/Parallax.cs
+++ game/Assets/Scripts/Environment/Parallax.cs
@@ -1,53 +1,80 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Renderer))]
 public class Parallax : MonoBehaviour
 {
-  [SerializeField] private Transform cameraTransform;
-  [SerializeField] private float parallaxFactor = 0.05f;
-  [SerializeField] private Vector2 offset;
-  [SerializeField] private bool lockY = false;
-  [SerializeField] private bool pixelSnap = true;
-  [SerializeField] private float pixelsPerUnit = 16f;
+    [Header("References")]
+    [SerializeField] private Transform cameraTransform;
 
-  private Material mat;
-  private Vector2 textureOffset;
-  private Vector3 initialCamPos;
+    [Header("Parallax Settings")]
+    [SerializeField] private float parallaxFactor = 0.05f;
+    [SerializeField] private Vector2 offset;
+    [SerializeField] private bool lockY = false;
 
-  void Start()
-  {
-    if (cameraTransform == null)
-      cameraTransform = Camera.main.transform;
+    [Header("Pixel Perfect")]
+    [SerializeField] private bool pixelSnap = true;
+    [SerializeField] private float pixelsPerUnit = 16f;
 
-    mat = GetComponent<Renderer>().material;
-    initialCamPos = cameraTransform.position;
+    [Header("Layer Sorting")]
+    [SerializeField] private string sortingLayer = "Default"; // e.g. "Background", "Midground", "Foreground"
+    [SerializeField] private int orderInLayer = 0;
 
-    transform.position = new Vector3(
-      transform.position.x + offset.x,
-      transform.position.y + offset.y,
-      transform.position.z
-    );
-  }
+    private Material mat;
+    private Vector2 textureOffset;
+    private Vector3 initialCamPos;
+    private Renderer rend;
 
-  void LateUpdate()
-  {
-    Vector3 camDelta = cameraTransform.position - initialCamPos;
-
-    textureOffset.x = camDelta.x * parallaxFactor;
-    textureOffset.y = 0; //camDelta.y * parallaxFactor;
-    mat.mainTextureOffset = textureOffset;
-
-    Vector3 targetPos = new Vector3(
-        cameraTransform.position.x + offset.x,
-        lockY ? transform.position.y : cameraTransform.position.y + offset.y,
-        transform.position.z
-    );
-
-    if (pixelSnap)
+    void Start()
     {
-      targetPos.x = Mathf.Round(targetPos.x * pixelsPerUnit) / pixelsPerUnit;
-      targetPos.y = Mathf.Round(targetPos.y * pixelsPerUnit) / pixelsPerUnit;
+        if (cameraTransform == null)
+            cameraTransform = Camera.main.transform;
+
+        rend = GetComponent<Renderer>();
+        mat = rend.material;
+        initialCamPos = cameraTransform.position;
+
+        // Apply sorting layer and order
+        rend.sortingLayerName = sortingLayer;
+        rend.sortingOrder = orderInLayer;
+
+        // Apply initial offset
+        transform.position = new Vector3(
+          transform.position.x + offset.x,
+          transform.position.y + offset.y,
+          transform.position.z
+        );
     }
 
-    transform.position = targetPos;
-  }
+    void LateUpdate()
+    {
+        Vector3 camDelta = cameraTransform.position - initialCamPos;
+
+        // Scrolling texture
+        textureOffset.x = camDelta.x * parallaxFactor;
+        textureOffset.y = 0; 
+        mat.mainTextureOffset = textureOffset;
+
+        // Target position based on camera
+        Vector3 targetPos = new Vector3(
+            cameraTransform.position.x + offset.x,
+            lockY ? transform.position.y : cameraTransform.position.y + offset.y,
+            transform.position.z
+        );
+
+        // Pixel snapping
+        if (pixelSnap)
+        {
+            targetPos.x = Mathf.Round(targetPos.x * pixelsPerUnit) / pixelsPerUnit;
+            targetPos.y = Mathf.Round(targetPos.y * pixelsPerUnit) / pixelsPerUnit;
+        }
+
+        transform.position = targetPos;
+    }
+
+    // Allow changing sorting at runtime
+    public void SetSorting(string layer, int order)
+    {
+        rend.sortingLayerName = layer;
+        rend.sortingOrder = order;
+    }
 }
